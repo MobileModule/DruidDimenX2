295e64fb955f1a47335473258b32ec5b184a9bec
lizhipeng <popularlzp@163.com>
2025-06-10 10:37:31 +0800

    升级版本

--- main/src/test/java/com/druid/main/ExampleUnitTest.java
+++ main/src/test/java/com/druid/main/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.druid.main.util.GenerateValueFiles;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,6 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+        GenerateValueFiles.buildPX();
     }
 }
